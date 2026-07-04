63766db54db42c96010c6c098ff24a8d1887d11e
Indraraj Singh <singhindraraj66@gmail.com>
2026-07-04 16:48:09 +0530

    commit controllers/reviews.js

--- routes/review.js
+++ routes/review.js
@@ -7,34 +7,17 @@ const Review = require('../models/review.js');
 const Listing = require('../models/listing.js');
 const {validateReview} = require("../middleware");
 
+const reviewControllers = require('../controllers/reviews.js');
+
 // review route
 // post route
-router.post("/" ,validateReview,wrapAsync (async(req,res)=>{
-    let listing =await Listing.findById(req.params.id);
-    let newReview =new Review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
+router.post("/" ,validateReview,wrapAsync(reviewControllers.createReview));
 
 
-    res.redirect(`/listings/${listing._id}`);
-
-}));
 
 // delete review route
 
 router.post("/:reviewId",
-    wrapAsync(async(req,res)=>{
-        let {id,reviewId} = req.params;
-        await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-        await Review.findByIdAndDelete(reviewId);
-
-        res.redirect(`/listings/${id}`);
-
- 
-}
-));
+    wrapAsync(reviewControllers.destroyReview));
 
 module.exports = router;
